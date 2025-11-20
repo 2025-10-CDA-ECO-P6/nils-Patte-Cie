f889fd8807c3a40c2fd3737bcdffa8ed2f7e3860
nilsw13 <nils.wenting@laplateforme.io>
2025-11-20 12:54:02 +0100

    REFACTO: refacto function name

--- frontend/src/app/animals/owner/store/animalStore.js
+++ frontend/src/app/animals/owner/store/animalStore.js
@@ -1,15 +1,15 @@
-
 import { create } from 'zustand';
 import animalsData from '../../../../../src/data/animalsOwner.json';
 
-export const useAnimalStore = create((set) => ({
+export const useOwnerAnimalStore = create((set) => ({
 
+    // etats
     owner: animalsData.data.proprietaire,
     animals: animalsData.data.animaux,
     selectedAnimal: null,
     isModalOpen: false,
 
-
+ // actions
     openModal: (animal) => set({
         selectedAnimal: animal,
         isModalOpen: true
