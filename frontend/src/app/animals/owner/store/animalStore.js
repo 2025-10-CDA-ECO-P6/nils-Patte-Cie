feb368a8966d13f58ee43468bf56569f19f9b8ba
nilsw13 <nils.wenting@laplateforme.io>
2025-11-20 11:21:15 +0100

    clean

--- frontend/src/app/animals/owner/store/animalStore.js
+++ frontend/src/app/animals/owner/store/animalStore.js
@@ -1,15 +1,15 @@
-// app/animals/owner/store/animalStore.js
+
 import { create } from 'zustand';
 import animalsData from '../../../../../src/data/animalsOwner.json';
 
 export const useAnimalStore = create((set) => ({
-    // ============ ÉTAT ============
+
     owner: animalsData.data.proprietaire,
     animals: animalsData.data.animaux,
     selectedAnimal: null,
     isModalOpen: false,
 
-    // ============ ACTIONS ============
+
     openModal: (animal) => set({
         selectedAnimal: animal,
         isModalOpen: true
