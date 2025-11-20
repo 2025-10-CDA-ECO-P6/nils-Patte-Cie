62756d28c23e2d37b89486ec16a2c6f62a89ad19
nilsw13 <nils.wenting@laplateforme.io>
2025-11-20 12:53:14 +0100

    REFACTO: Modified component usage to include new type params

--- frontend/src/Components/ClientWrappers/OwnerPageClient.jsx
+++ frontend/src/Components/ClientWrappers/OwnerPageClient.jsx
@@ -1,23 +1,23 @@
 'use client'
-import {useAnimalStore} from "@/app/animals/owner/store/animalStore";
+import {useOwnerAnimalStore} from "@/app/animals/owner/store/animalStore";
 import AnimalsList from "@/Components/AnimalsList";
 import AnimalModal from "@/Components/AnimalModal";
 
 function OwnerPageClient() {
-    const owner = useAnimalStore((state) => state.owner);
-    const isModalOpen = useAnimalStore((state) => state.isModalOpen);
-    const selectedAnimal = useAnimalStore((state) => state.selectedAnimal);
+    const owner = useOwnerAnimalStore((state) => state.owner);
+    const isModalOpen = useOwnerAnimalStore((state) => state.isModalOpen);
+    const selectedAnimal = useOwnerAnimalStore((state) => state.selectedAnimal);
 
     return (
         <div className="page-container">
             <h1>Bonjour {owner.first_name}</h1>
 
             {!isModalOpen && (
-                <AnimalsList/>
+                <AnimalsList type='owner'/>
             )}
 
             {isModalOpen && selectedAnimal && (
-                <AnimalModal/>
+                <AnimalModal type='owner'/>
             )}
 
         </div>
