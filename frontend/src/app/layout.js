9579b6c9e746242bb6a74260602ab4574c7725e9
nilsw13 <nils.wenting@laplateforme.io>
2025-11-21 14:48:05 +0100

    Modifiy style + font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Manrope } from "next/font/google";
 import "./globals.css";
+import MobilNavbar from "@/Components/MobilNavbar/MobilNavbar";
+import Header from "@/Components/Header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const manrope = Manrope({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -19,8 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={manrope.className}>
+      <Header/>
+      {children}
+       <MobilNavbar/>
+
       </body>
     </html>
   );
