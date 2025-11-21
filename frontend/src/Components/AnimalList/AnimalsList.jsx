3f236339e839a3b1e2f4965d4ce73357d50000cc
nilsw13 <nils.wenting@laplateforme.io>
2025-11-21 14:50:19 +0100

    DEV: Finish animal list for now

--- frontend/src/Components/AnimalList/AnimalsList.jsx
+++ frontend/src/Components/AnimalList/AnimalsList.jsx
@@ -10,7 +10,10 @@ function AnimalsList() {
 
     return (
         <div className={styles.listContainer}>
-            <h2 className={styles.pageTitle}>Bonjour {owner.first_name + ' ' + owner.last_name} </h2>
+           <div className={styles.greetContainer}>
+               <h2 className={styles.pageTitle}>Bonjour {owner.first_name + ' ' + owner.last_name} </h2>
+               <span>Retrouver tout les infos sur vos animaux</span>
+           </div>
             <div className={styles.cardsGrid}>
                 {animals.map((animal) => (
                     <HealthRecordPresentationCard
