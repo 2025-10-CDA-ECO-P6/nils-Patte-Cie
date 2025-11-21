1807c4b45a84643f1bcba06549e1f9b9ed98460d
nilsw13 <nils.wenting@laplateforme.io>
2025-11-21 14:53:48 +0100

    FIX : add format hour function but need to be somewhere else better :) '

--- frontend/src/app/animals/owner/store/animalStore.js
+++ frontend/src/app/animals/owner/store/animalStore.js
@@ -1,5 +1,11 @@
-import { create } from 'zustand';
-import animalsData from '../../../../../src/data/animalsOwner.json';
+    import { create } from 'zustand';
+    import animalsData from '../../../../../src/data/animalsOwner.json';
+
+
+export const formatDate = (dateString) => {
+    if (!dateString) return 'N/A';
+    return new Date(dateString).toLocaleDateString('fr-FR');
+};
 
 export const useOwnerAnimalStore = create((set) => ({
 
@@ -9,14 +15,19 @@ export const useOwnerAnimalStore = create((set) => ({
     selectedAnimal: null,
     isModalOpen: false,
 
- // actions
-    openModal: (animal) => set({
-        selectedAnimal: animal,
-        isModalOpen: true
-    }),
 
-    closeModal: () => set({
-        selectedAnimal: null,
-        isModalOpen: false
-    }),
-}));
+     // actions
+        openModal: (animal) => set({
+            selectedAnimal: animal,
+            isModalOpen: true
+        }),
+
+        closeModal: () => set({
+            selectedAnimal: null,
+            isModalOpen: false
+        }),
+
+
+
+
+    }));
